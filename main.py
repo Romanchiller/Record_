a988af44265b9e06ec1dcca9707bb0e5fa45ea95
Roman Tarabrin <VacuuM2007@yandex.ru>
2023-01-24 21:25:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+x = {}
+
+
+f = open('1.txt','rt', encoding='utf-8')
+result = f.readlines()
+x[len(result)] = [f'{f.name}\n']
+x[len(result)] += result
+f.close()
+
+f = open('2.txt','rt', encoding='utf-8')
+result = f.readlines()
+x[len(result)] = [f'{f.name}\n']
+x[len(result)] += result
+f.close()
+
+f = open('3.txt','rt', encoding='utf-8')
+result = f.readlines()
+x[len(result)] = [f'{f.name}\n']
+x[len(result)] += result
+f.close()
+
+sorted_dict = dict(sorted(x.items()))
+
+
+with open('super.txt', 'a',encoding='utf-8') as s:
+    for key, value in sorted_dict.items():
+        s.write(value[0])
+        s.write(f'{str(key)}\n')
+        s.writelines(value[1:])
+        s.write('\n')
+s.close()
+
+
+
+print(x)
+print(result)
+# f = open('super.txt', 'w', encoding='utf-8')
+# f.writelines(x.sorted())
+# f.close()
